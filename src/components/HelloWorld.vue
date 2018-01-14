41cbe87c51fb186b7d07205f2ddae8011bd08dd9
FengZikun <fengzikun11@163.com>
2018-01-14 21:27:39 +0800

    01-14
    day1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '欢迎来到大西瓜商城'
+    }
+  },
+  computed:{
+    ...mapState({
+      'count':state=>state.count
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store/main.js
@@ -0,0 +1,7 @@
+const main = {
+	state:{
+		count:1
+	}
+}
+
+export default main
